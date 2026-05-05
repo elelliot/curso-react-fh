1f1ad9c8d67eac6dfec396ca526277dced9d17bc
ElElliot <elliot158094@gmail.com>
2026-05-05 02:53:35 -0700

    - Reinstalls React Vite project without Babel-React Compiler

--- first-steps/vite.config.ts
+++ first-steps/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vitest/config";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react()],
   // Config para testing (cambiemos el `defineConfig` de `vite` por el de `vitest/config`)... al correr el test, nos pedira instalar 'jsdom', damos que si
   test: {
     environment: "jsdom",
