85504a039c9ffb216e29e332b3ba8c10baa7f288
ElElliot <elliot158094@gmail.com>
2026-05-06 22:29:12 -0700

    - Handle search updates previous searches

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -6,14 +6,26 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(["cyberpunk 2077"]);
+  const [previousTerms, setPreviousTerms] = useState(["cyberpunk2077"]);
 
   const handleTermClicked = (term: string) => {
     console.log({ term });
   };
 
   const handleSearch = (query: string) => {
-    console.log({ query });
+    // Remove whitespaces on start and end, and go lowercase
+    const term = query.trim().toLowerCase();
+    // Query shouldn't be empty (first step helps for this)
+    if (!term) return;
+
+    // If term is already included in previousItems, then we do nothing
+    if (previousTerms.includes(term)) return;
+
+    // Otherwise we set it to the start of array, only 8 items can be shown so we remove the last item after adding the new one
+    setPreviousTerms((prevItems) => {
+      prevItems = [term, ...prevItems];
+      return prevItems.slice(0, 8);
+    });
   };
   return (
     <>
