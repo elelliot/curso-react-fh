b11c0af16ffbce93f43bcb47c4992d8d9faf31ee
ElElliot <elliot158094@gmail.com>
2026-05-19 16:50:29 -0700

    - Creates useOptimistic exersice

---
+++ 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
@@ -0,0 +1,122 @@
+import { useOptimistic, useState } from "react";
+
+interface Comment {
+  id: number;
+  text: string;
+  optimistic?: boolean;
+}
+let lastId = 2;
+
+export const InstagromApp = () => {
+  const [comments, setComments] = useState<Comment[]>([
+    { id: 1, text: "¡Gran foto!" },
+    { id: 2, text: "Me encanta 🧡" },
+  ]);
+
+  /* NOTE: Sirve para aplicar updates en la UI a partir de un `state`. 
+  Con esto asumimos que el server va dar una respuesta positiva. 
+  En caso de fallar, podemos hacer un rollback hacia el state anterior.
+
+  Mandamos 2 args, 
+  -El `state` al que queremos updatear de forma optimista
+  - Dispatch function con el que retornas el nuevo state
+    La funcion acepta 2 args 
+    -> El estado actual (como el useState que tambien se le dice el prevValue)
+    -> Y el nuevo valor con el que actualizamos el `state`
+
+  Se desestructura como `useState` y funciona en esa parte igual
+  
+  TLDR: Hacemos un optimistic UI update antes de llamar al server, y una vez se resuelva correctamente,
+  actualizamos el state
+  */
+  const [optimisticComments, addOptimisticComment] = useOptimistic(
+    comments,
+    (currentComments, newComment: string) => {
+      lastId++;
+      return [
+        ...currentComments,
+        {
+          id: lastId,
+          text: newComment,
+          optimistic: true,
+        },
+      ];
+    },
+  ); // React 19 >
+
+  // FormData es un objeto del form con los campos
+  const handleAddComment = async (formData: FormData) => {
+    // Accedemos mediante la key para obtener el valor del campo
+    const messageText = formData.get("post-message") as string; // Lo ideal es validar con Zod en vez de Castear por que el tipo es `FormDataEntryValue | null`
+    console.log(messageText);
+
+    // Antes de llamar al server, suponemos que el server responde bien y actualizamos la UI
+    addOptimisticComment(messageText);
+
+    // Simulamos http request
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log("Server answered");
+
+    setComments((prev) => [
+      ...prev,
+      {
+        id: new Date().getTime(),
+        text: messageText, //Como no mandamos el optimistic, no se ve `enviando` ya que aqui es donde se actualiza la UI al final
+      },
+    ]);
+  };
+
+  return (
+    <div className="bg-slate-700 h-screen flex flex-col items-center justify-center">
+      {/* Post de ejemplo */}
+      <div className="flex flex-col items-center justify-center bg-gray-300 rounded-t-3xl p-4 w-[500px]">
+        <img
+          src="https://images.unsplash.com/photo-1649972904349-6e44c42644a7?w=500&h=500&fit=crop"
+          alt="Instagrom"
+          className="object-cover rounded-xl mb-4"
+        />
+        <p className="text-black font-bold mb-4">
+          Mira que interesante esta funcionalidad de la API de React.
+        </p>
+      </div>
+
+      {/* Comentarios */}
+      <ul className="flex flex-col items-start justify-center bg-gray-300 w-[500px] p-4">
+        {/* {comments.map((comment) => ( */}
+        {optimisticComments.map((comment) => (
+          <li key={comment.id} className="flex items-center gap-2 mb-2">
+            <div className="bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center">
+              <span className="text-white text-center">A</span>
+            </div>
+            <p className="text-black">{comment.text}</p>
+            {comment.optimistic && (
+              <span className="text-gray-500 text-sm">enviando... </span>
+            )}
+          </li>
+        ))}
+      </ul>
+
+      {/* Formulario de comentarios */}
+      <form
+        action={handleAddComment}
+        className="flex flex-col items-center justify-center bg-gray-300 w-[500px] rounded-b-3xl p-4"
+      >
+        <input
+          type="text"
+          name="post-message"
+          placeholder="Escribe un comentario"
+          required
+          className="w-full p-2 rounded-md mb-2 text-black bg-white"
+        />
+        <button
+          type="submit"
+          disabled={false}
+          className="bg-blue-500 text-white p-2 rounded-md w-full"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MemoCounter } from "./06-memos/MemoCounter";
+import { InstagromApp } from "./07-useOptimistic/InstagromApp";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MemoCounter />
+    <InstagromApp />
   </StrictMode>,
 );
