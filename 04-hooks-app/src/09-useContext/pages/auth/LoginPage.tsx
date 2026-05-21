814a76a2a424fb8b7b6a4236c1b8a1acd1349ba2
ElElliot <elliot158094@gmail.com>
2026-05-20 18:33:29 -0700

    - Mocks a login with context consuming

--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,15 +1,41 @@
+import { useContext, useState, type SubmitEvent } from "react";
+import { Link, useNavigate } from "react-router";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router";
+import { UserContext } from "@/09-useContext/context/UserContext";
+import { toast } from "sonner";
 
 export const LoginPage = () => {
+  const { login } = useContext(UserContext); //* Consumimos el Context que creamos
+
+  const [userId, setUserId] = useState("");
+
+  const navigation = useNavigate();
+
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = login(+userId);
+    if (!result) {
+      toast.error("Usuario no encontrado");
+      return;
+    }
+
+    navigation("/profile");
+  };
+
   return (
     <div className="flex flex-col items-center min-h-screen">
       <h1 className="text-4xl font-bold">Iniciar Sesion</h1>
       <hr />
 
-      <form className="flex flex-col gap-2 my-10">
-        <Input type="numbner" placeholder="ID del Usuario" />
+      <form className="flex flex-col gap-2 my-10" onSubmit={handleSubmit}>
+        <Input
+          type="numbner"
+          placeholder="ID del Usuario"
+          value={userId}
+          onChange={(event) => setUserId(event.target.value)}
+        />
 
         <Button type="submit">Login</Button>
 
