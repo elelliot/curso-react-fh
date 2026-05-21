def6decbfc761fca16fed7b4072fcb8f8db358cf
ElElliot <elliot158094@gmail.com>
2026-05-20 17:12:20 -0700

    - Creates pages design

--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Iniciar Sesion</h1>
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="numbner" placeholder="ID del Usuario" />
+
+        <Button type="submit">Login</Button>
+
+        <Link to="/about">
+          <Button variant="ghost">Volver a la pagina principal</Button>
+        </Link>
+      </form>
+    </div>
+  );
 };
