1bfdf30fc29c1043bd7ddfbb5f0ee422247879b4
ElElliot <elliot158094@gmail.com>
2026-05-25 17:08:33 -0700

    - Creates Heroes and Admin Layouts, adds lazy loading

--- 05-heroes-app/src/router/app.router.tsx
+++ 05-heroes-app/src/router/app.router.tsx
@@ -1,24 +1,54 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
+
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/search/SearchPage";
+// import { SearchPage } from "@/heroes/search/SearchPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+// import { AdminLayout } from "@/heroes/layouts/AdminLayout";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const SearchPage = lazy(() => import("@/heroes/search/SearchPage")); // NOTE: Lazy loaded route (need to default export our route, but we'll get a eslint error)
+// eslint-disable-next-line react-refresh/only-export-components
+const AdminLayout = lazy(() => import("@/heroes/layouts/AdminLayout"));
+
+//! Forma antigua de lazy load
+// const SearchPage = lazy(() =>
+//   import("@/heroes/search/SearchPage").then((module) => ({
+//     default: module.SearchPage,
+//   })),
+// );
 
 export const appRouter = createBrowserRouter([
+  // Layout
   {
     path: "/",
-    element: <HomePage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true, // Es la pagina principal del layout, no ocupamos poner el `path`
+        element: <HomePage />,
+      },
+      {
+        path: "heroes/1", // Tampoco ocupamos poner el slash ya que el padre lo provee
+        element: <HeroPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
   },
-  {
-    path: "heroes/1",
-    element: <HeroPage />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
-  },
+
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ]);
