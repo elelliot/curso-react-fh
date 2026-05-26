91c024b76fa876a713ccb161219c19e46d587cc2
ElElliot <elliot158094@gmail.com>
2026-05-25 17:36:27 -0700

    - Installs components - Creates JumboTron reusable component - Adds asset

--- 05-heroes-app/src/heroes/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/search/SearchPage.tsx
@@ -1,5 +1,14 @@
+import { CustomJumboTron } from "@/components/custom/CustomJumboTron";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumboTron
+        title="Busqueda de SuperHeroes"
+        description="Descubre, explora y administra super heroes y villanos"
+      />
+    </>
+  );
 };
 
 export default SearchPage;
