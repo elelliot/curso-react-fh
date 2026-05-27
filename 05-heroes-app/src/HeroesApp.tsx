3e211dc71113a218b0e50e3ecbac0d7c195b7502
ElElliot <elliot158094@gmail.com>
2026-05-26 17:49:52 -0700

    - Installs axios, creates instance - Installs react query, creates instance and provider - Creates get heroes by page action and creates axios instance - Creates Heroes Types and interfaces

--- 05-heroes-app/src/HeroesApp.tsx
+++ 05-heroes-app/src/HeroesApp.tsx
@@ -1,10 +1,18 @@
 import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { appRouter } from "./router/app.router";
+
+// 1- La instancia de react query
+const queryClient = new QueryClient();
 
 export const HeroesApp = () => {
   return (
-    <>
+    // 2- Debemos wrapear la app en el <QueryClientProvider /> con nuestro `queryClient`
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={appRouter} />
-    </>
+      {/* 3- Las devtools */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
--- 05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CustomJumboTron } from "@/components/custom/CustomJumboTron";
 import { CustomBreadCrumbs } from "@/components/custom/CustomBreadCrumbs";
 import { CustomPagination } from "@/components/custom/CustomPagination";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { HeroGrid } from "@/heroes/components/HeroGrid";
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action";
 
 export const HomePage = () => {
   const [activeTab, setActiveTab] = useState<
     "all" | "favorites" | "heroes" | "villains"
   >("all");
+
+  // ! No tenemos `state` ni error handling y peor, se ejecuta cada que se monta el componente... React Query es la clave
+  // useEffect(() => {
+  //   getHeroesByPage().then((heroes) => console.log({ heroes }));
+  // }, []);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["heroes"],
+    queryFn: () => getHeroesByPageAction(),
+    staleTime: 1000 * 60 * 5, // 5 Minutos para que no haga nuevas peticiones y nos devuelva el cache antes de que se vuelva obsoleta la data
+  });
+
+  console.log({ data });
+  console.log({ isLoading });
 
   return (
     <>
