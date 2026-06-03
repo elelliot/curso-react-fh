a70e600c30eb2cf091f7ca8460cb33193ea48d7c
ElElliot <elliot158094@gmail.com>
2026-06-02 23:26:13 -0700

    - Adds shadcn components - Adds assets - Creates mock data file - Starts Products Grid

--- 07-teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
 import { CustomJumboTron } from "@/shop/components/CustomJumboTron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumboTron title="Todos los Productos" />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
     </>
   );
 };
