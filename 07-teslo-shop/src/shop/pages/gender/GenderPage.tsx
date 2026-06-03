f44b16c3ce6d0fc22d4b20f763d5d22d80df8cee
ElElliot <elliot158094@gmail.com>
2026-06-03 16:43:44 -0700

    - Updates button classes - Implem,ents navigation for gender - Adds login and admin buttons - Gender page now shows title based on url params

--- 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,21 @@
+import { useParams } from "react-router";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { CustomJumboTron } from "@/shop/components/CustomJumboTron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { products } from "@/mocks/products.mock";
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+
+  return (
+    <>
+      <CustomJumboTron title={`Productos para ${genderLabel}`} />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
