611358acb267e15e933dbbaeb8f31150a451354d
ElElliot <elliot158094@gmail.com>
2026-06-02 21:27:28 -0700

    - Adds shadcn  input component - Creates custom components

--- 07-teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { CustomJumboTron } from "@/shop/components/CustomJumboTron";
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1 className="font-montserrat">Nah Bruh</h1>
-      <h1 className="font-montserrat font-thin">Nah Bruh</h1>
-      <h1 className="font-montserrat font-normal">Nah Bruh</h1>
-      <h1 className="font-montserrat font-bold">Nah Bruh</h1>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <CustomJumboTron title="Todos los Productos" />
+    </>
   );
 };
