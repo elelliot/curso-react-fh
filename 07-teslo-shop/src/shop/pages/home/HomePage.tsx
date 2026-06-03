ecd5e2a54afde5f1bac9bfb4dbbbe1f323cca953
ElElliot <elliot158094@gmail.com>
2026-06-02 21:12:32 -0700

    Installs google montserrat font

--- 07-teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -3,7 +3,10 @@ import { Button } from "@/components/ui/button";
 export const HomePage = () => {
   return (
     <div>
-      HomePage
+      <h1 className="font-montserrat">Nah Bruh</h1>
+      <h1 className="font-montserrat font-thin">Nah Bruh</h1>
+      <h1 className="font-montserrat font-normal">Nah Bruh</h1>
+      <h1 className="font-montserrat font-bold">Nah Bruh</h1>
       <Button>Click me</Button>
     </div>
   );
