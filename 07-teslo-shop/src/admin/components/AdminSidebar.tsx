d1fa454f82534bc210b432bfcbc17c7716b10019
ElElliot <elliot158094@gmail.com>
2026-06-03 20:05:24 -0700

    - Adds some links to navigate - creates helper function to see active route - Fix type

--- 07-teslo-shop/src/admin/components/AdminSidebar.tsx
+++ 07-teslo-shop/src/admin/components/AdminSidebar.tsx
@@ -12,6 +12,7 @@ import {
   ChevronRight,
 } from "lucide-react";
 import { CustomLogo } from "@/components/custom/CustomLogo";
+import { Link, useLocation } from "react-router";
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -22,16 +23,23 @@ export const AdminSidebar: React.FC<SidebarProps> = ({
   isCollapsed,
   onToggle,
 }) => {
+  const { pathname } = useLocation();
+
   const menuItems = [
-    { icon: Home, label: "Dashboard", active: true },
-    { icon: BarChart3, label: "Analytics" },
-    { icon: Users, label: "Users" },
-    { icon: ShoppingCart, label: "Orders" },
-    { icon: FileText, label: "Reports" },
-    { icon: Bell, label: "Notifications" },
-    { icon: Settings, label: "Settings" },
-    { icon: HelpCircle, label: "Help" },
+    { icon: Home, label: "Dashboard", to: "/admin" },
+    { icon: BarChart3, label: "Productos", to: "/admin/products" },
+    { icon: Users, label: "Usuarios" },
+    { icon: ShoppingCart, label: "Ordenes" },
+    { icon: FileText, label: "Reportes" },
+    { icon: Bell, label: "Notificaciones" },
+    { icon: Settings, label: "Ajustes" },
+    { icon: HelpCircle, label: "Ayuda" },
   ];
+
+  const isActiveRoute = (to: string) => {
+    //TODO: Ajustarlo cuando estemos en la pantalla del producto
+    return pathname === to;
+  };
 
   return (
     <div
@@ -57,10 +65,10 @@ export const AdminSidebar: React.FC<SidebarProps> = ({
             const Icon = item.icon;
             return (
               <li key={index}>
-                <a
-                  href="#"
+                <Link
+                  to={item.to || "/admin"}
                   className={`flex items-center space-x-3 px-3 py-2 rounded-lg transition-all duration-200 group ${
-                    item.active
+                    isActiveRoute(item.to || "/xxxx")
                       ? "bg-blue-50 text-blue-600 border-r-2 border-blue-600"
                       : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
                   }`}
@@ -69,7 +77,7 @@ export const AdminSidebar: React.FC<SidebarProps> = ({
                   {!isCollapsed && (
                     <span className="font-medium">{item.label}</span>
                   )}
-                </a>
+                </Link>
               </li>
             );
           })}
