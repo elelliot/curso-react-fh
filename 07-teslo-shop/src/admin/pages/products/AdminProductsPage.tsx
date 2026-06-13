67006d68e0824d4f402da24e788a891320529ff9
ElElliot <elliot158094@gmail.com>
2026-06-12 18:49:48 -0700

    - Creates currency format helper - Admin Header Search now works with Products - Admin products page now shows api products

--- 07-teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
+++ 07-teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { PlusIcon } from "lucide-react";
+import { PencilIcon, PlusIcon } from "lucide-react";
 import { AdminTitle } from "@/admin/components/AdminTitle";
 import { CustomPagination } from "@/components/custom/CustomPagination";
 import { Button } from "@/components/ui/button";
@@ -11,62 +11,84 @@ import {
   TableBody,
   TableCell,
 } from "@/components/ui/table";
+import { useProducts } from "@/shop/hooks/useProducts";
+import { Spinner } from "@/components/ui/spinner";
+import { currencyFormatter } from "@/lib/currency-formatter";
 
 export const AdminProductsPage = () => {
-  return (
-    <>
-      <div className="flex justify-between items-center">
-        <AdminTitle
-          title="Productos"
-          subtitle="Aqui puedes ver y administrar tus productos"
-        />
+  const { data, isLoading } = useProducts();
 
-        <div className="flex justify-end mb-10 gap-4">
-          <Link to="/admin/products/new">
-            <Button>
-              <PlusIcon />
-              Nuevo Producto
-            </Button>
-          </Link>
+  if (isLoading) return <Spinner className="size-12" />;
+
+  if (data)
+    return (
+      <>
+        <div className="flex justify-between items-center">
+          <AdminTitle
+            title="Productos"
+            subtitle="Aqui puedes ver y administrar tus productos"
+          />
+
+          <div className="flex justify-end mb-10 gap-4">
+            <Link to="/admin/products/new">
+              <Button>
+                <PlusIcon />
+                Nuevo Producto
+              </Button>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <Table className="bg-white p-10 shadow-2xs border-gray-200 mb-10">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Imagen</TableHead>
-            <TableHead>Nombre</TableHead>
-            <TableHead>Precio</TableHead>
-            <TableHead>Categoría</TableHead>
-            <TableHead>Inventario</TableHead>
-            <TableHead>Tallas</TableHead>
-            <TableHead className="text-right">Acciones</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>
-              <img
-                src="https://placehold.co/250x250"
-                alt="Product"
-                className="w-20 h-20 object-cover rounded-md"
-              />
-            </TableCell>
-            <TableCell>Producto 1</TableCell>
-            <TableCell>$250.00</TableCell>
-            <TableCell>Categoría 1</TableCell>
-            <TableCell>100 stock</TableCell>
-            <TableCell>XS,S,L</TableCell>
-            <TableCell className="text-right">
-              <Link to={`/admin/products/t-shirt-teslo`}>Editar</Link>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+        <Table className="bg-white p-10 shadow-2xs border-gray-200 mb-10">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Imagen</TableHead>
+              <TableHead>Nombre</TableHead>
+              <TableHead>Precio</TableHead>
+              <TableHead>Categoría</TableHead>
+              <TableHead>Inventario</TableHead>
+              <TableHead>Tallas</TableHead>
+              <TableHead className="text-right">Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>
+                  <img
+                    src={product.images[0]}
+                    alt={product.slug}
+                    className="w-20 h-20 object-cover rounded-md"
+                  />
+                </TableCell>
 
-      <CustomPagination totalPages={10} />
-    </>
-  );
+                <TableCell>
+                  <Link
+                    to={`/admin/products/${product.id}`}
+                    className="hover:text-blue-600 underline"
+                  >
+                    {product.title}
+                  </Link>
+                </TableCell>
+                <TableCell>{currencyFormatter(product.price)}</TableCell>
+                <TableCell className="capitalize">{product.gender}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>{product.sizes.join(", ")}</TableCell>
+                <TableCell className="text-right">
+                  <Link to={`/admin/products/${product.id}`}>
+                    <Button variant="outline">
+                      <PencilIcon className="text-blue-500" />
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        {data.products.length > 0 && (
+          <CustomPagination totalPages={data.pages} />
+        )}
+      </>
+    );
 };
